68e7b5710c410205606de08f5aa6998da732e985
edo979 <prezentabilan@gmail.com>
2023-05-31 07:43:25 +0200

    change description of some pages

--- src/consts.ts
+++ src/consts.ts
@@ -1,2 +1,3 @@
 export const SITE_TITLE = 'Programiranje'
-export const SITE_DESCRIPTION = 'Welcome to my website!'
+export const SITE_DESCRIPTION =
+  'Naučite programirati internet stranice uz upotrebu HTML, CSS i javaScript programskog jezika. Stranica je namjenjena početnicima. Na stranici možete pronaći tekstove o: HTML, CSS i javaScript, primjere kako napraviti dijelove internet stranice ili cijele stranice, vijesti vezane za web tehnologije.'
